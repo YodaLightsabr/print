5dbd5352dfbc7c812a68c65f07132f112bed126d
yodalightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2024-05-10 13:29:44 -0700

    🖨️

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
+      <title>Print</title>
+      <link rel="favicon" href="/favicon.png" />
+      <link rel="shortcut icon" href="/favicon.png" />
+      <link rel="icon" href="/favicon.png" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
